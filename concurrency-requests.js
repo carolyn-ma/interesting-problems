be385353c9a0cb6c48b486c0fe1a91e56046b275
Carolyn Ma <carolyn.x.ma@gmail.com>
2018-06-14 14:17:43 -0700

    Update concurrency-requests.js

--- concurrency-requests.js
+++ concurrency-requests.js
@@ -1,16 +1,16 @@
-function simpleRequest(callback) {
+function sampleRequest(callback) {
     setTimeout(() => {
       callback();
     }, Math.random()*1000);
 }
 
-function loadBalancer(concurrency) {
+function miniLoadBalancer(concurrency) {
     this.concurrency = concurrency;
     this.requestPool = [];
     this.activeRequests = 0;
 }
 
-loadBalancer.prototype.newRequest = function(request) {
+miniLoadBalancer.prototype.newRequest = function(request) {
     this.requestPool.push(request);
     while (this.requestPool.length > 0) {
         if (this.activeRequests < this.concurrency) {
